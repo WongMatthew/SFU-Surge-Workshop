0c638e6fc3dc46cebf2445db24dc77eb35b8a99b
Matt Wong <wongmatthew357@gmail.com>
2023-03-10 03:16:57 -0800

    remove files

--- ReactJS #1 Workshop/webdev-website/index.js
+++
@@ -1,9 +0,0 @@
-const jokeButton = document.getElementById("joke-btn");
-
-jokeButton.addEventListener("click", function () {
-  alert("A little (h)otter!");
-});
-
-// jokeButton.onclick = function () {
-//   alert("A little (h)otter! :)");
-// };
